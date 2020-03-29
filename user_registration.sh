d383d8a7d562a002269d7cdab1d83987270bf76a
sayalipawade <sayalipawade05@gmail.com>
2020-03-29 11:15:40 +0530

    Added:Checking the first name is valid or not

--- user_registration.sh
+++ user_registration.sh
@@ -1,2 +1,15 @@
 #!/bin/bash -x
 echo "welcome"
+
+#constants
+VALIDPATTERN="^[A-Z][a-z]{2,}$"
+
+read -p "Enter first name:" firstName
+
+if [[ $firstName =~ $VALIDPATTERN ]]
+then
+	echo "Valid first name"
+else
+	echo "Not valid first name"
+fi
+
